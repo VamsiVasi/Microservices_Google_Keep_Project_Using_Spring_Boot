f974febb6bdf6e4c3cb27e75aaa8737ba7646f0a
VamsiVasi <vasikrish999@gmail.com>
2022-03-29 19:08:01 +0530

    Microservices Google Keep Project Using Spring Boot And MongoDB

---
+++ Microservice_Google_Keep/User_Microservice/src/main/java/com/albanero/User_Microservice/UserMicroserviceApplication.java
@@ -0,0 +1,22 @@
+package com.albanero.User_Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class UserMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroserviceApplication.class, args);
+	}
+
+	@Bean
+	public WebClient.Builder getWebClient()
+	{
+		return WebClient.builder();
+		
+	}
+
+}
